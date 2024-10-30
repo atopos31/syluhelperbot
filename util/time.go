f8c70a6acd9e59591d782a7369b82119f17edfeb
hackerxiao <2945294768@qq.com>
2024-10-30 13:08:37 +0800

    feat: 竞赛通知优化

---
+++ util/time.go
@@ -0,0 +1,11 @@
+package util
+
+import "time"
+func IsToday(dateStr string) (bool, error) {
+	date, err := time.Parse(time.DateTime, dateStr)
+	if err != nil {
+		return false, err
+	}
+	today := time.Now()
+	return date.Year() == today.Year() && date.Month() == today.Month() && date.Day() == today.Day(), nil
+}
---
+++ util/time_test.go
@@ -0,0 +1,11 @@
+package util
+
+import "testing"
+
+func TestTime(t *testing.T) {
+	ok,err  := IsToday("2024-10-31 00:00:00")
+	if err != nil {
+		t.Error(err)
+	}
+	t.Log(ok)
+}
